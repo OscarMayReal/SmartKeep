0258af97f8849ae2c5587b01bb3167092df179e5
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-10-27 18:03:49 +0100

    some final changes again

--- smartkeep-client/app/app/page.tsx
+++ smartkeep-client/app/app/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 import { QuickActions } from "@/components/dashboard";
 import { motion } from "framer-motion";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-    return (
-        <motion.div initial={{ x: 50 }} animate={{ x: 0 }} transition={{ duration: 0.15, ease: "easeOut" }} className="page-layout">
-            <div className="page-header">
-                <div>
-                    <h1 className="page-header-title">Home</h1>
-                    <h2 className="page-header-subtitle">Get an overview of your inventory</h2>
-                </div>
-            </div>
-            <QuickActions/>
-        </motion.div>
-    );
+    // return (
+    //     <motion.div initial={{ x: 50 }} animate={{ x: 0 }} transition={{ duration: 0.15, ease: "easeOut" }} className="page-layout">
+    //         <div className="page-header">
+    //             <div>
+    //                 <h1 className="page-header-title">Home</h1>
+    //                 <h2 className="page-header-subtitle">Get an overview of your inventory</h2>
+    //             </div>
+    //         </div>
+    //         <QuickActions/>
+    //     </motion.div>
+    // );
+    return redirect("/app/inventory");
 }
